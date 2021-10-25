c1ed26fffa00177487ebbbed31f3c51bf8cd9fc6
matt.jourard <mjourard@gmail.com>
2021-10-24 20:40:59 -0400

    got local hot reloading working again and figured out where the volume is coming from

--- src/handler.ts
+++ src/handler.ts
@@ -4,8 +4,8 @@ export const hello: APIGatewayProxyHandler = async (event, _context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
+      message: 'This is a typescript serverless lambda function!',
+      path: event.path,
     }, null, 2),
   };
 }
